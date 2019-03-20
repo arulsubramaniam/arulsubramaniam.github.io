eec036520ff4635f398c5030354e035dbc2ede7c
arulsubramaniam <arulsubramaniam.tech@gmail.com>
2019-03-20 13:36:08 -0400

    Testing spa

---
+++ precache-manifest.bf6714695bd4e5e30a03dd05bfdeb8e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dc86390420b21ab3a7ab",
+    "url": "/static/js/main.81429266.chunk.js"
+  },
+  {
+    "revision": "5016ea763fe003952387",
+    "url": "/static/js/2.a8394e1f.chunk.js"
+  },
+  {
+    "revision": "dc86390420b21ab3a7ab",
+    "url": "/static/css/main.1fe7bdab.chunk.css"
+  },
+  {
+    "revision": "34e0dafe2f244aa1de5da5786bc39f2f",
+    "url": "/index.html"
+  }
+];
