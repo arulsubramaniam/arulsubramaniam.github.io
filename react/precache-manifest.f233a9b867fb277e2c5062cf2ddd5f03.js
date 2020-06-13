3eefbb7b8d697a66c27a8e304124e969a5ef510c
Arul Subramaniam <arulsubramaniam.tech@gmail.com>
2020-06-13 11:55:50 -0400

    fix static path for react

--- react/precache-manifest.f233a9b867fb277e2c5062cf2ddd5f03.js
+++ react/precache-manifest.f233a9b867fb277e2c5062cf2ddd5f03.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/react/static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "4f974c8321961a763c22",
-    "url": "/static/js/main.83c4efdd.chunk.js"
+    "url": "/react/static/js/main.83c4efdd.chunk.js"
   },
   {
     "revision": "c84feed04cbba07131fa",
-    "url": "/static/js/2.233c0c4c.chunk.js"
+    "url": "/react/static/js/2.233c0c4c.chunk.js"
   },
   {
     "revision": "4f974c8321961a763c22",
-    "url": "/static/css/main.1fe7bdab.chunk.css"
+    "url": "/react/static/css/main.1fe7bdab.chunk.css"
   },
   {
     "revision": "bf48629243c146352ae636ee0a5eac3d",
